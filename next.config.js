3c802e473bb4dd63cb4b4650916c5ce97bb81572
Daichi Kato <daichi5967@docomo.ne.jp>
2023-03-25 14:34:55 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  assetPrefix: process.env.GITHUB_ACTIONS && "/portfolio",
   basePath: process.env.GITHUB_ACTIONS && "/portfolio",
   trailingSlash: true,
   reactStrictMode: true,
