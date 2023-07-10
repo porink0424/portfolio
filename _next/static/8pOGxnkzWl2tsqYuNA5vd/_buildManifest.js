704128a19e2ff465a4ddf25610e613b3c85570cf
porinky0424 <porinky0424@users.noreply.github.com>
2023-07-10 14:45:06 +0000

    deploy: 9d9c4789fc95ad29caa3cc5089d39b2c7d9fedf3

--- _next/static/8pOGxnkzWl2tsqYuNA5vd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8fcc8ac091ead9e6.css","static/chunks/pages/index-24b1c51013837b36.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
